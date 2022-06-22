dbbd99c0ba1585dea2ca06a6f89cf79a7cb52fd3
M J <69307802+whitehat-1@users.noreply.github.com>
2022-06-22 04:18:19 -0700

    new

--- I4G004098KPB/blog/admin.py
+++ I4G004098KPB/blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-class Post():
-    
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
